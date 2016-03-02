4d34fb8e4f0c346bd753bfb49a55719b3b81d56d
alu0100767803 <alu0100767803@ull.edu.es>
2016-03-02 20:35:05 +0000

    Añadidos los to

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -26,6 +26,16 @@
   Celsius.prototype = new Temperatura();
   Celsius.prototype.constructor = Celsius;
 
+  Celsius.prototype.toFarenheit = function () {
+    var result = (this.valor * 9/5)+32;
+    return result;
+  }
+
+Celsius.prototype.toKelvin = function () {
+    var result = this.valor + 273.15;
+    return result;
+}
+
   function Farenheit(valor)
   {
     Temperatura.call(this, valor);
@@ -34,19 +44,39 @@
   Farenheit.prototype = new Temperatura();
   Farenheit.prototype.constructor = Farenheit;
 
+  Farenheit.prototype.toCelsius = function () {
+    var result = (this.valor - 32) * 5/9;
+    return result;
+  }
+
+  Farenheit.prototype.toKelvin = function () {
+    var result = ((this.valor - 32) / (9/5)) + 273.15;
+    return result;
+  }
+
   function Kelvin(valor)
   {
     Temperatura.call(this, valor, "k");
   }
 
-Kelvin.prototype = new Temperatura();
-Kelvin.prototype.constructor = Kelvin;
+  Kelvin.prototype = new Temperatura();
+  Kelvin.prototype.constructor = Kelvin;
+
+  Kelvin.prototype.toCelsius = function () {
+    var result = this.valor - 273.15;
+    return result;
+  }
+
+  Kelvin.prototype.toFarenheit = function () {
+    var result = ((this.valor - 273.15) * 9/5) + 32;
+    return result;
+  }
 
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
   exports.Kelvin = Kelvin;
-  
+
   exports.convertir = function() {
     var valor     = document.getElementById('convert').value,
         elemento  = document.getElementById('converted'),
