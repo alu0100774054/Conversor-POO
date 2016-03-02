283d34b55e3ecdaa05ca0d0ee05513a7a28abfeb
alu0100767803 <alu0100767803@ull.edu.es>
2016-03-02 20:29:05 +0000

    Creada la herencia

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,39 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
+    this.valor = valor;
+    this.tipo = tipo || "";
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
   }
-  
+
   function Temperatura(valor,tipo)
   {
+    Media.call(this, valor, tipo);
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
   }
-  
+
+  Temperatura.prototype = new Medida();
+  Temperatura.prototype.constructor = Temperatura;
+
   function Celsius(valor)
   {
+    Temperatura.call(this, valor);
   }
-  
+
+  Celsius.prototype = new Temperatura();
+  Celsius.prototype.constructor = Celsius;
+
   function Farenheit(valor)
   {
+    Temperatura.call(this, valor);
   }
-  
+
+  Farenheit.prototype = new Temperatura();
+  Farenheit.prototype.constructor = Farenheit;
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -30,14 +44,14 @@
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
     valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,8 +61,9 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
+          elemento.innerHTML = "ERROR! Intenta algo como '23.2C' ";
           /* rellene este código */
       }
     }
